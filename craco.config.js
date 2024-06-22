3dd8e7fed876e3e8a28aa33441f1c092e4a4280f
Godfrey Huang <godfrey.Huang@lincolnuni.ac.nz>
2024-06-22 20:32:36 +1200

    Update eslint config

--- craco.config.js
+++ craco.config.js
@@ -1,28 +1,19 @@
 module.exports = {
   style: {},
   webpack: {},
-  eslint: {
+  eslint: {},
+  jest: {
     configure: {
-      parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-      parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
-        ecmaFeatures: {
-          jsx: true, // Allows for the parsing of JSX
-        },
-      },
-      extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
-      setting: {
-        react: {
-          version: 'detect',
-        },
-      },
-      rules: {
-        '@typescript-eslint/no-unused-vars': 'off',
-      },
+      coveragePathIgnorePatterns: [
+        'src/setupTests.ts',
+        'src/mocks/.*',
+        'src/main/.*',
+        '.styled.tsx',
+        'src/reportWebVitals.ts',
+        'src/index.tsx',
+      ],
     },
   },
-  jest: {},
   babel: {},
   devServer: {},
 };
